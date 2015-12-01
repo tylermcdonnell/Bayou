32382b42a182d7843aaf8c4f53159ab471e4ea37
Tyler McDonnell <tyler@cs.utexas.edu>
2015-12-01 11:46:02 -0600

    Added basic client skeleton.

---
+++ bayou_handout/client/Client.java
@@ -0,0 +1,20 @@
+package client;
+
+/**
+ * Abstraction for Bayou client.
+ * @author tsm
+ *
+ */
+public class Client implements Runnable {
+	
+	public Client()
+	{
+		
+	}
+	
+	@Override
+	public void run()
+	{
+		
+	}
+}
