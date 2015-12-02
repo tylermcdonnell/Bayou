f46ac666ba5cfe413f3a239d8c47526aa564fff3
Tyler McDonnell <tyler@cs.utexas.edu>
2015-12-01 19:15:09 -0600

    Added skeleton for WriteLog.

---
+++ bayou_handout/server/WriteLog.java
@@ -0,0 +1,25 @@
+package server;
+
+import java.util.ArrayList;
+
+import message.Write;
+
+/**
+ * Bayou Write Log. This log manages both committed and tentative writes.
+ * @author tsm
+ *
+ */
+public class WriteLog {
+
+	public ArrayList<Write> getCommittedWrites()
+	{
+		// TODO
+		return new ArrayList<Write>();
+	}
+	
+	public ArrayList<Write> getTentativeWrites()
+	{
+		// TODO
+		return new ArrayList<Write>();
+	}
+}
