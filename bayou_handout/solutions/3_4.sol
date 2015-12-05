328c31b3cc0ed977a2165c28ef6ce0420449e5fa
Tyler McDonnell <tyler@cs.utexas.edu>
2015-12-05 17:39:10 -0600

    Added test.

---
+++ bayou_handout/solutions/3_4.sol
@@ -0,0 +1,10 @@
+hello:world
+hello:goodbye
+PUT:("hello, world"):TRUE
+PUT:("hello, goodbye"):FALSE
+PUT:("hello, world"):TRUE
+PUT:("hello, goodbye"):TRUE
+PUT:("hello, world"):TRUE
+PUT:("hello, goodbye"):TRUE
+hello:goodbye
+hello:goodbye
